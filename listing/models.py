cf48d51dbb1fc264e76cd333903fa2c67c4c611d
amykochu <amal@labglo.com>
2018-05-30 01:00:35 +0530

    Mandate & Opportunity - Models updated

--- listing/models.py
+++ listing/models.py
@@ -140,11 +140,11 @@ class SubSector(models.Model):
 
 class HighLevelFinancial(models.Model):
     """ """
-    revenue = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Revenue', blank=True, null=True)
-    costs = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Costs', blank=True, null=True)
-    ebitda = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='EBITDA', blank=True, null=True)
-    capex = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='CAPEX', blank=True, null=True)
-    net_profit = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Net Profit', blank=True, null=True)
+    revenue = models.DecimalField(max_digits=15, decimal_places=4, verbose_name='Revenue', blank=True, null=True)
+    costs = models.DecimalField(max_digits=15, decimal_places=4, verbose_name='Costs', blank=True, null=True)
+    ebitda = models.DecimalField(max_digits=15, decimal_places=4, verbose_name='EBITDA', blank=True, null=True)
+    capex = models.DecimalField(max_digits=15, decimal_places=4, verbose_name='CAPEX', blank=True, null=True)
+    net_profit = models.DecimalField(max_digits=15, decimal_places=4, verbose_name='Net Profit', blank=True, null=True)
 
 
 class Opportunity(models.Model):
@@ -156,11 +156,12 @@ class Opportunity(models.Model):
     selling_by = models.CharField(max_length=500, verbose_name='Who is selling? Why?', blank=True, null=True)
     competitors = models.CharField(max_length=500, verbose_name='Major competitors', blank=True, null=True)
     valuation = models.ForeignKey(ValuationFundTicket,  on_delete=models.SET_NULL, null=True, verbose_name='Valuation - metrics and rationale')
-    amount_invested = models.DecimalField(max_digits=5, decimal_places=2,
+    valuation_text = models.CharField(max_length=200, verbose_name='Valuation - metrics and rationale (Input)', blank=True, null=True)
+    amount_invested = models.DecimalField(max_digits=15, decimal_places=4,
                                           verbose_name='Amount invested to date ($USm)', blank=True, null=True)
-    ownership_structure = models.DecimalField(max_digits=5, decimal_places=2, verbose_name='Ownership structure (%)', blank=True, null=True)
-    ebitda = models.FloatField(verbose_name='Company EBITDA ($USm)', blank=True, null=True)
-    break_even_year = models.DecimalField(max_digits=5, decimal_places=2, verbose_name='Break-even year', blank=True, null=True)
+    ownership_structure = models.DecimalField(max_digits=15, decimal_places=4, verbose_name='Ownership structure (%)', blank=True, null=True)
+    ebitda = models.DecimalField(max_digits=15, decimal_places=4, verbose_name='Company EBITDA ($USm)', blank=True, null=True)
+    break_even_year = models.DecimalField(max_digits=15, decimal_places=4, verbose_name='Break-even year', blank=True, null=True)
     est_payback = models.ManyToManyField(EstPayback, verbose_name='Est. payback on raise')
     size_ticket_total = models.ManyToManyField(ValuationFundTicket, verbose_name='Size ticket total available',
                                                related_name='opportunity_size_ticket')
@@ -170,29 +171,29 @@ class Opportunity(models.Model):
     #
     sector = models.ForeignKey(Sector, on_delete=models.SET_NULL, verbose_name='Sector', blank=True, null=True)
     sub_sector = models.ManyToManyField(SubSector, verbose_name='Sub Sector')
-    yield_select = models.ManyToManyField(Yield, verbose_name='Yield')
-    return_estimate = models.DecimalField(max_digits=5, decimal_places=2, verbose_name='Return estimate (3-yr) (%)', blank=True, null=True)
-    growth_expectation_year1 = models.CharField(max_length=500, verbose_name='Earnings growth expectation (%) Year 1', blank=True, null=True)
-    growth_expectation_year2 = models.CharField(max_length=500, verbose_name='Earnings growth expectation (%) Year 2', blank=True, null=True)
-    growth_expectation_year3 = models.CharField(max_length=500, verbose_name='Earnings growth expectation (%) Year 3', blank=True, null=True)
+    yield_select = models.ForeignKey(Yield, on_delete=models.SET_NULL, verbose_name='Yield', null=True)
+    return_estimate = models.DecimalField(max_digits=15, decimal_places=4, verbose_name='Return estimate (3-yr) (%)', blank=True, null=True)
+    growth_expectation_year1 = models.DecimalField(max_digits=15, decimal_places=4, verbose_name='Earnings growth expectation (%) Year 1', blank=True, null=True)
+    growth_expectation_year2 = models.DecimalField(max_digits=15, decimal_places=4, verbose_name='Earnings growth expectation (%) Year 2', blank=True, null=True)
+    growth_expectation_year3 = models.DecimalField(max_digits=15, decimal_places=4, verbose_name='Earnings growth expectation (%) Year 3', blank=True, null=True)
     class_select = models.ManyToManyField(Class, verbose_name='Class')
     series_stage = models.ManyToManyField(SeriesStage, verbose_name='Series/stage')
-    investor_required = models.ManyToManyField(InvestorSpecial, verbose_name='Lead Investor required in place')
-    amount_lead_partner = models.DecimalField(max_digits=5, decimal_places=2, verbose_name='Amount injected by Lead Partner', blank=True, null=True)
-    amount_other_partner = models.DecimalField(max_digits=5, decimal_places=2, verbose_name='Amount injected by Other Partners', blank=True, null=True)
-    amount_weraise = models.DecimalField(max_digits=5, decimal_places=2, verbose_name='Amount available for WeRaise', blank=True, null=True)
+    investor_required = models.ForeignKey(InvestorSpecial, on_delete=models.CASCADE, verbose_name='Lead Investor required in place', null=True)
+    amount_lead_partner = models.DecimalField(max_digits=15, decimal_places=4, verbose_name='Amount injected by Lead Partner', blank=True, null=True)
+    amount_other_partner = models.DecimalField(max_digits=15, decimal_places=4, verbose_name='Amount injected by Other Partners', blank=True, null=True)
+    amount_weraise = models.DecimalField(max_digits=15, decimal_places=4, verbose_name='Amount available for WeRaise', blank=True, null=True)
     investment_offered = models.ManyToManyField(InvestmentOffered, verbose_name='Type of investment offered')
     offer = models.ManyToManyField(Offer, verbose_name='Offer')
-    special_situation = models.ManyToManyField(InvestorSpecial, verbose_name='Special situation',
-                                               related_name='special_situation')
+    special_situation = models.ForeignKey(InvestorSpecial, on_delete=models.CASCADE, verbose_name='Special situation',
+                                               related_name='special_situation', null=True)
     financials = models.ForeignKey(Financial, on_delete=models.SET_NULL, verbose_name='Financials', null=True)
     #
     Revenue = models.CharField(max_length=500, verbose_name='Revenue', blank=True, null=True)
-    estimated_irr = models.DecimalField(max_digits=5, decimal_places=2, verbose_name='Estimated IRR', blank=True, null=True)
+    estimated_irr = models.DecimalField(max_digits=15, decimal_places=4, verbose_name='Estimated IRR', blank=True, null=True)
     exit_timing = models.CharField(max_length=500, verbose_name='Expected exit timing', blank=True, null=True)
     use_of_funds = models.CharField(max_length=500, verbose_name='Use of funds', blank=True, null=True)
-    deadline_commitment = models.DecimalField(max_digits=5, decimal_places=2, verbose_name='Deadline for formal commitment', blank=True, null=True)
-    deadline_legal = models.DecimalField(max_digits=5, decimal_places=2, verbose_name='Deadline for execution of legal docs', blank=True, null=True)
+    deadline_commitment = models.DecimalField(max_digits=15, decimal_places=4, verbose_name='Deadline for formal commitment', blank=True, null=True)
+    deadline_legal = models.DecimalField(max_digits=15, decimal_places=4, verbose_name='Deadline for execution of legal docs', blank=True, null=True)
     proposed_process = models.CharField(max_length=500, verbose_name='Proposed process', blank=True, null=True)
     proposed_exit = models.CharField(max_length=500, verbose_name='Proposed exit options', blank=True, null=True)
     financial_statements = models.FileField(upload_to='listing/documents/',
@@ -221,21 +222,21 @@ class Mandate(models.Model):
     fund_size = models.ManyToManyField(ValuationFundTicket, verbose_name='Required company or fund size')
     size_ticket_total = models.ManyToManyField(ValuationFundTicket, verbose_name='Desired ticket size',
                                                related_name='mandate_size_ticket')
-    percentage_company = models.DecimalField(max_digits=5, decimal_places=2, verbose_name='% of company (min - max)')
+    percentage_company = models.DecimalField(max_digits=15, decimal_places=4, verbose_name='% of company (min - max)')
     #
     geography = models.ForeignKey(Geography, on_delete=models.SET_NULL, null=True)
     country = models.ManyToManyField(Country)
     #
     sector = models.ForeignKey(Sector,on_delete=models.SET_NULL, verbose_name='Sector', blank=True, null=True)
     sub_sector = models.ManyToManyField(SubSector, verbose_name='Sub Sector')
-    yield_select = models.ManyToManyField(Yield, verbose_name='Yield')
+    yield_select = models.ForeignKey(Yield, on_delete=models.CASCADE, verbose_name='Yield', null=True)
     #
-    growth_expectation_year1 = models.CharField(max_length=500, verbose_name='Earnings growth expectation (%) Year 1', blank=True, null=True)
-    growth_expectation_year2 = models.CharField(max_length=500, verbose_name='Earnings growth expectation (%) Year 2', blank=True, null=True)
-    growth_expectation_year3 = models.CharField(max_length=500, verbose_name='Earnings growth expectation (%) Year 3', blank=True, null=True)
+    growth_expectation_year1 = models.DecimalField(max_digits=15, decimal_places=4, verbose_name='Earnings growth expectation (%) Year 1', blank=True, null=True)
+    growth_expectation_year2 = models.DecimalField(max_digits=15, decimal_places=4, verbose_name='Earnings growth expectation (%) Year 2', blank=True, null=True)
+    growth_expectation_year3 = models.DecimalField(max_digits=15, decimal_places=4, verbose_name='Earnings growth expectation (%) Year 3', blank=True, null=True)
     class_select = models.ManyToManyField(Class, verbose_name='Class')
     series_stage = models.ManyToManyField(SeriesStage, verbose_name='Series/stage')
-    investor_required = models.ManyToManyField(InvestorSpecial, verbose_name='Lead Investor required in place')
+    investor_required = models.ForeignKey(InvestorSpecial, on_delete=models.CASCADE, verbose_name='Lead Investor required in place', null=True)
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
 
