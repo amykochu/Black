8e8259505c64190cc1cdcec500eec4cac4a72fcd
amal <amal@labglo.com>
2018-07-09 16:43:19 +0530

    V2 db - User model - update

--- black/urls.py
+++ black/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-# from listing import views
+from listing import views
 from django.conf.urls.static import static
 from django.conf import settings
 from django.contrib.auth import views as auth_views
@@ -24,17 +24,17 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('login/', auth_views.login, name='login'),
     path('logout/', auth_views.logout, name='logout'),
-    # path('', views.DashboardHome.as_view(), name='home'),
+    path('', views.DashboardHome.as_view(), name='home'),
     path('admin/', admin.site.urls),
-    # path('opportunity-upload/', views.OpportunityUploadView.as_view(), name='upload'),
+    path('opportunity-upload/', views.OpportunityUploadView.as_view(), name='upload'),
     # path('search/', views.SearchView.as_view(), name='search'),
-    # path('opportunity-detail/<int:pk>/', views.OpportunityDetailView.as_view(), name='opportunity-detail-view'),
+    path('opportunity-detail/<int:pk>/', views.OpportunityDetailView.as_view(), name='opportunity-detail-view'),
     # path('mandate-detail/<int:pk>/', views.MandateDetailView.as_view(), name='mandate-detail-view'),
-    # path('mandate-upload/', views.MandateUploadView.as_view(), name='mandate_upload'),
+    path('mandate-upload/', views.MandateUploadView.as_view(), name='mandate_upload'),
     # path('fund-seeking/', views.AllMandate.as_view(), name='mandate-all'),
     # path('opportunity/', views.AllOpportunity.as_view(), name='opportunity-all'),
 
-    # path('ajax/load-geography/', views.load_cities, name='ajax_load_geography'),
-    # path('ajax/load-sector/', views.load_sectors, name='ajax_load_sector'),
+    path('ajax/load-geography/', views.load_cities, name='ajax_load_geography'),
+    path('ajax/load-sector/', views.load_sectors, name='ajax_load_sector'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
               + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
