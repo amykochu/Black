c1a50fca6c19b6294d95ac2499a09dc5473ce7b0
amal <amal@labglo.com>
2018-07-11 12:39:35 +0530

    Removed Add new from User Inline mandate Hide fields password, groups, user permissions, last login

--- accounts/admin.py
+++ accounts/admin.py
@@ -7,6 +7,9 @@ class MandateInline(admin.StackedInline):
     model = Mandate
     extra = 0
 
+    def has_add_permission(self, request):
+        return False
+
 
 class UserAdmin(admin.ModelAdmin):
     inlines = [
@@ -14,6 +17,6 @@ class UserAdmin(admin.ModelAdmin):
     ]
     list_display = ('id', 'email', 'first_name', 'last_name', 'title', 'location', 'country', 'fund', 'mobile',
                     'land_line', )
-
+    exclude = ('password', 'last_login', 'groups', 'user_permissions')
 
 admin.site.register(User, UserAdmin)
