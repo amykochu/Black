71de9294476039dc1bb8ef422b04503f6d60542a
amal <amal@labglo.com>
2018-07-17 15:09:47 +0530

    User model - User type added

--- accounts/admin.py
+++ accounts/admin.py
@@ -15,8 +15,9 @@ class UserAdmin(admin.ModelAdmin):
     inlines = [
         MandateInline,
     ]
-    list_display = ('id', 'email', 'first_name', 'last_name', 'title', 'location', 'country', 'fund', 'mobile',
-                    'land_line', )
+    list_display = ('id', 'email', 'first_name', 'last_name', 'title', 'location', 'country', 'fund', 'mobile_code',
+                    'mobile', 'land_line_code', 'land_line', 'user_type', 'is_active')
     exclude = ('password', 'last_login', 'groups', 'user_permissions')
+
 
 admin.site.register(User, UserAdmin)
